a193a83080fc20212d5977a802445b7dc8e7c24b
Zach Sarvas <zachsarvas@yahoo.com>
2022-08-31 21:02:03 -0700

    added desc

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to the Versus Bot Leaderboard!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Versus bot is the shit
         </p>
       </main>
 
