f6b8caa5b210fb2c8fa85e29a0006be4fcf0bbfb
Zach Sarvas <zachsarvas@yahoo.com>
2022-09-16 23:46:20 -0700

    table attempt 8

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,23 @@ export default function Home() {
 
 <h2>A basic HTML table</h2>
 
-
+<table style="width:100%">
+  <tr>
+    <th>Company</th>
+    <th>Contact</th>
+    <th>Country</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+  </tr>
+</table>
 
 </body>
         <p className="description">
