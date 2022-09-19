0afb2d14c3ea8252a32e8723d2e578611bd64f43
Zach Sarvas <zachsarvas@yahoo.com>
2022-09-19 13:27:28 -0700

    setup environment variables

--- components/db.js
+++ components/db.js
@@ -1,6 +1,16 @@
 // Initialize the JS client
 import { createClient } from '@supabase/supabase-js'
-const supabase = createClient("https://zywthnmeikffxbzusxkb.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp5d3Robm1laWtmZnhienVzeGtiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjI2NzIxODYsImV4cCI6MTk3ODI0ODE4Nn0.g8Nam0FhgnGb2-NFH3eGLc-GvUBuXBfE2RwtutKh6Zo")
+import { data } from 'cypress/types/jquery'
+require('dotenv').config()
+const envVariables = process.env;
+
+// Read vars from envVariables object
+const {
+    SUPABASE_URL,
+    SUPABASE_KEY
+} = envVariables;
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
 
 // Make a request
 const { data: rocketleague, error } = await supabase.from('rocketleague').select('*')
+
--- pages/index.js
+++ pages/index.js
@@ -13,28 +13,28 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to the Versus Bot Leaderboard!" />
+        <Header title="Versus Bot Leaderboard!" />
         <body>
         <table>
-  <tr>
-    <th>Name</th>
-    <th>MMR</th>
-    <th>Wins</th>
-    <th>Losses</th>
-  </tr>
-  <tr>
-    <td>Alfreds Futterkiste</td>
-    <td>1000</td>
-    <td>0</td>
-    <td>4</td>
-  </tr>
-  <tr>
-    <td>Centro comercial Moctezuma</td>
-    <td>1040</td>
-    <td>4</td>
-    <td>0</td>
-  </tr>
-</table> 
+          <tr>
+            <th>Name</th>
+            <th>MMR</th>
+            <th>Wins</th>
+            <th>Losses</th>
+         </tr>
+         <tr>
+            <td>Alfreds Futterkiste</td>
+            <td>1000</td>
+            <td>0</td>
+            <td>4</td>
+          </tr>
+         <tr>
+            <td>Centro comercial Moctezuma</td>
+            <td>1040</td>
+            <td>4</td>
+            <td>0</td>
+          </tr>
+        </table> 
       
    </body>
         <p className="description">
