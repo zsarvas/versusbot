01cb6b8aedda589feaee3c028f362ee03b85f89f
Zach Sarvas <zachsarvas@yahoo.com>
2022-09-19 14:11:13 -0700

    testing literal injection 1

--- pages/index.js
+++ pages/index.js
@@ -15,37 +15,10 @@ export default function Home() {
       <main>
         <Header title="Versus Bot Leaderboard" />
         <body>
-        <table>
-          <tr>
-            <th>Name</th>
-            <th>MMR</th>
-            <th>Wins</th>
-            <th>Losses</th>
-            <th>+/-</th>
-            <th>Win Percentage</th>
-         </tr>
-         <tr>
-            <td>Alfreds Futterkiste</td>
-            <td>1000</td>
-            <td>0</td>
-            <td>4</td>
-            <td>4</td>
-            <td>4</td>
-          </tr>
-         <tr>
-            <td>Centro comercial Moctezuma</td>
-            <td>1040</td>
-            <td>4</td>
-            <td>0</td>
-            <td>0</td>
-            <td>0</td>
-          </tr>
-        </table> 
+        
       
    </body>
         <p className="description">
-          Powered by Versus Bot
-          
         </p>
         
         <script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js"></script>
