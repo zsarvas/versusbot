61c776c2e61b964c3a478293fb7011eacee18b5b
Zach Sarvas <zachsarvas@yahoo.com>
2022-09-19 21:50:18 -0700

    possibly working table 6

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-import infoCall from './db'
 
 export default function Home() { 
   return (
